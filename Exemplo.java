b60bb33fcce6a8350d947c412bdb78e04cb4980e
Mkelman18 <160253161+Mkelman18@users.noreply.github.com>
2025-04-04 12:11:32 -0300

    Add files via upload

---
+++ Exemplo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exemplo {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.n);
+
+        Aluno aluno1 = new Aluno();
+
+        aluno1.nome = "Augusto";
+        aluno1.sobrenome = "Guimarães";
+        aluno1.idade = "Masculino";
+        aluno1.cpf = "123.456.789";
+        aluno1.ra = "123";
+        aluno1.filiacao = "pai e mãe";
+
+        aluno1.dataNascimento = new Date(year:2000, month:3, date:14);
+
+        Endereco enderecoDoAluno2 = new ();
+
+
+        sc.close();
+
+    }
+    
+
+    
+}
